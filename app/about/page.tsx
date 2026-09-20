119fc5feca8c897514de93c1c02f25dee0d2d274
jaymaxx6702-arch <jaymaxx6702@gmail.com>
2026-09-20 16:37:14 +0530

    Add About Yash Laser page

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import { InfoPage } from "@/components/InfoPage";
+
+export const metadata = {
+  title: "About Yash Laser",
+};
+
+export default function Page() {
+  return <InfoPage page="about" />;
+}
