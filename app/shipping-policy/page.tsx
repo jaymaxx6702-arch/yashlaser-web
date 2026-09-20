1c75e907bb40cf674aeaea894ecea0d2f02e6023
jaymaxx6702-arch <jaymaxx6702@gmail.com>
2026-09-20 16:37:23 +0530

    Add Shipping policy page

---
+++ app/shipping-policy/page.tsx
@@ -0,0 +1,9 @@
+import { InfoPage } from "@/components/InfoPage";
+
+export const metadata = {
+  title: "Shipping policy",
+};
+
+export default function Page() {
+  return <InfoPage page="shipping" />;
+}
