86b8e466dbf1958acd52b4a8f3372cd633d6e813
jaymaxx6702-arch <jaymaxx6702@gmail.com>
2026-09-20 16:41:29 +0530

    Protect localized customization routes

--- app/robots.ts
+++ app/robots.ts
@@ -16,7 +16,7 @@ const privatePaths = [
 
 export default function robots(): MetadataRoute.Robots {
   const localizedPrivate = ["gu", "hi", "mr"].flatMap((lang) =>
-    ["/cart", "/checkout", "/track-order", "/account"].map(
+    ["/cart", "/checkout", "/track-order", "/account", "/customize"].map(
       (path) => "/" + lang + path,
     ),
   );
