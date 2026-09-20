9882697e5435ed35bfa541b617ba02b26e50bd83
jaymaxx6702-arch <jaymaxx6702@gmail.com>
2026-09-20 16:37:18 +0530

    Add Frequently asked questions page

---
+++ app/faq/page.tsx
@@ -0,0 +1,9 @@
+import { InfoPage } from "@/components/InfoPage";
+
+export const metadata = {
+  title: "Frequently asked questions",
+};
+
+export default function Page() {
+  return <InfoPage page="faq" />;
+}
