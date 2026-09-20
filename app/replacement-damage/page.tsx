13d19d004cd919f8f41d9f62cc4f67f226526ff9
jaymaxx6702-arch <jaymaxx6702@gmail.com>
2026-09-20 16:37:31 +0530

    Add Replacement & damage policy page

---
+++ app/replacement-damage/page.tsx
@@ -0,0 +1,9 @@
+import { InfoPage } from "@/components/InfoPage";
+
+export const metadata = {
+  title: "Replacement & damage policy",
+};
+
+export default function Page() {
+  return <InfoPage page="replacement" />;
+}
